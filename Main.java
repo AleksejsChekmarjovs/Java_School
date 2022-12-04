36f4f217718470e1dbcc55f24a51b3a623114ec2
Aleksejs Cekmarjovs <117226224+Cheefeer@users.noreply.github.com>
2022-12-04 19:15:53 +0200

    Add files via upload
    //        First level: Разработать программу для управления "Умным домом".
    //        С клавиатуры вводится:
    //
    //        текущее время (в часах от 0 до 23)
    ////        текущий день недели
    ////       включена ли сигнализация
    
    //        "Умный дом" должен реализовать следующие сценарии:
    //        1) В будний день в 8ч поднять шторы, если хозяева дома (сигнализация отключена)
    //        2) В выходной день в 11ч поднять шторы, если хозяева дома
    //        3) В будний день в 20ч вечера включить чайник, если хозяев нет дома
    //        4) В будний день в 23ч опустить шторы, если шторы были подняты
    //        Выполнение действия обозначать выводом соответствующего сообщения на экран.

--- Main.java
+++ Main.java
@@ -1,45 +1,66 @@
+
+//
+//        First level: Разработать программу для управления "Умным домом".
+//        С клавиатуры вводится:
+//
+//        текущее время (в часах от 0 до 23)
+////        текущий день недели
+////       включена ли сигнализация
+
+//        "Умный дом" должен реализовать следующие сценарии:
+//        1) В будний день в 8ч поднять шторы, если хозяева дома (сигнализация отключена)
+//        2) В выходной день в 11ч поднять шторы, если хозяева дома
+//        3) В будний день в 20ч вечера включить чайник, если хозяев нет дома
+//        4) В будний день в 23ч опустить шторы, если шторы были подняты
+//        Выполнение действия обозначать выводом соответствующего сообщения на экран.
+
+
 import java.util.Scanner;
 
 public class Main {
+
+
     public static void main(String[] args) {
 
+        Scanner scTime = new Scanner(System.in);
+        System.out.println("Enter time 0 - 24: ");
+        int time = scTime.nextInt();
+
+        Scanner scWeekDays = new Scanner(System.in);
+        System.out.println("Enter day of week 1 - 7: ");
+        int weekDays = scWeekDays.nextInt();
+
+        Scanner csSignalization = new Scanner(System.in);
+        System.out.println("Enter 1 if signalization is ON or 2 if signalization is OFF:");
+        int signalization = csSignalization.nextInt();
+
+        boolean signalizationOn = signalization < 2;
 
 
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("введите 1 если вводите в градусах цельсия или 2 если в фаренгейта: ");
-        int CH = sc.nextInt();
-        if (CH == 1) {
 
 
 
 
+        boolean kettleIsHot = time >= 20 && weekDays < 6 != signalizationOn;
 
-                Scanner scanner = new Scanner(System.in);
-            System.out.println("Введите температуру в градусах цельсия :");
-                double getThempInC = scanner.nextDouble();
-                double tempF = getThempInC * 9 / 5 + 32;
-            long roundedNumber = Math.round(tempF);
-                System.out.println(roundedNumber + " В градусах фаренгейт");
+        boolean curtainsUp = ((time >= 8 && time < 23 && weekDays < 6) != signalizationOn) || (time >= 11 && weekDays > 5 != signalizationOn);
 
-
-        } else if (CH == 2) {
-
-
-                Scanner scanner = new Scanner(System.in);
-            System.out.println("Введите температуру в градусах фаренгейт :");
-                double getThempInF = scanner.nextDouble();
-                double tempC = (getThempInF - 32) * 5 / 9;
-                long roundedNumber = Math.round(tempC);
-                System.out.println(roundedNumber + " В градусах цельсия");
-
-
-
-        } else {
-            System.out.println("вы ввели неправильное значение");
-        }
-
-
+if (signalizationOn){
+    System.out.println("Nobody is home");
+}else {
+    System.out.println("sombody is home");
+}
+if (kettleIsHot){
+    System.out.println("Kettle is hot");
+}else {
+    System.out.println("Kettle is cold");
+}
+if (curtainsUp){
+    System.out.println("Curtains are Up");
+}else {
+    System.out.println("Curtains are closed");
+}
 
 
     }
